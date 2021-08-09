8f2b3ca8b5211825d02e6e5977494b103bd14b74
J. E. Minick <james.minick@prodigy.net>
2021-08-09 08:19:14 -0700

    Removed spaces on the GitHub icon link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ function Footer(props) {
         <a href={props.fburl} alt="facebook url" className="footer-icons" target="blank"><i className="fa fa-facebook"></i></a>
         <a href="https://twitter.com/EdofSD" alt="twitter url" className="footer-icons" target="blank"><i className="fa fa-twitter"></i></a>
         <a href="https://www.linkedin.com/in/james-minick-sandiego/" alt="linkedin url" className="footer-icons" target="blank"><i className="fa fa-linkedin"></i></a>
-        <a href="https://github.com/JEMinick?tab=repositories/" alt="Github url" className="footer-icons" target="blank"> <i className="fa fa-github"></i> </a>
+        <a href="https://github.com/JEMinick?tab=repositories/" alt="Github url" className="footer-icons" target="blank"><i className="fa fa-github"></i></a>
       </div>
     </footer>
   );
